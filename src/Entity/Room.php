189e18d3fb60e4feafd7521ea4f0152795f79de4
emup <emuppet@protonmail.com>
2020-05-20 12:59:58 +0200

    Added Room entity and crud

---
+++ src/Entity/Room.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\RoomRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=RoomRepository::class)
+ */
+class Room
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\ManyToOne(targetEntity=RoomType::class, inversedBy="rooms")
+     */
+    private $roomtypeId;
+
+    /**
+     * @ORM\Column(type="float")
+     */
+    private $price;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getRoomtypeId(): ?RoomType
+    {
+        return $this->roomtypeId;
+    }
+
+    public function setRoomtypeId(?RoomType $roomtypeId): self
+    {
+        $this->roomtypeId = $roomtypeId;
+
+        return $this;
+    }
+
+    public function getPrice(): ?float
+    {
+        return $this->price;
+    }
+
+    public function setPrice(float $price): self
+    {
+        $this->price = $price;
+
+        return $this;
+    }
+}
--- src/Entity/RoomType.php
+++ src/Entity/RoomType.php
@@ -3,6 +3,8 @@
 namespace App\Entity;
 
 use App\Repository\RoomTypeRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 
 /**
@@ -26,6 +28,16 @@ class RoomType
      * @ORM\Column(type="float")
      */
     private $price_markup;
+
+    /**
+     * @ORM\OneToMany(targetEntity=Room::class, mappedBy="roomtypeId")
+     */
+    private $rooms;
+
+    public function __construct()
+    {
+        $this->rooms = new ArrayCollection();
+    }
 
     public function getId(): ?int
     {
@@ -52,6 +64,37 @@ class RoomType
     public function setPriceMarkup(float $price_markup): self
     {
         $this->price_markup = $price_markup;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection|Room[]
+     */
+    public function getRooms(): Collection
+    {
+        return $this->rooms;
+    }
+
+    public function addRoom(Room $room): self
+    {
+        if (!$this->rooms->contains($room)) {
+            $this->rooms[] = $room;
+            $room->setRoomtypeId($this);
+        }
+
+        return $this;
+    }
+
+    public function removeRoom(Room $room): self
+    {
+        if ($this->rooms->contains($room)) {
+            $this->rooms->removeElement($room);
+            // set the owning side to null (unless already changed)
+            if ($room->getRoomtypeId() === $this) {
+                $room->setRoomtypeId(null);
+            }
+        }
 
         return $this;
     }
