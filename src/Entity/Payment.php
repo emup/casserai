042890cd9ddaed005b3784a031982806a75f9d8e
emup <emuppet@protonmail.com>
2020-05-20 12:50:22 +0200

    depression 2.0

---
+++ src/Entity/Payment.php
@@ -0,0 +1,92 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\PaymentRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=PaymentRepository::class)
+ */
+class Payment
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $userId;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $type;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $cardnumber;
+
+    /**
+     * @ORM\Column(type="date")
+     */
+    private $payment_date;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getUserId(): ?int
+    {
+        return $this->userId;
+    }
+
+    public function setUserId(int $userId): self
+    {
+        $this->userId = $userId;
+
+        return $this;
+    }
+
+    public function getType(): ?string
+    {
+        return $this->type;
+    }
+
+    public function setType(string $type): self
+    {
+        $this->type = $type;
+
+        return $this;
+    }
+
+    public function getCardnumber(): ?string
+    {
+        return $this->cardnumber;
+    }
+
+    public function setCardnumber(string $cardnumber): self
+    {
+        $this->cardnumber = $cardnumber;
+
+        return $this;
+    }
+
+    public function getPaymentDate(): ?\DateTimeInterface
+    {
+        return $this->payment_date;
+    }
+
+    public function setPaymentDate(\DateTimeInterface $payment_date): self
+    {
+        $this->payment_date = $payment_date;
+
+        return $this;
+    }
+}
